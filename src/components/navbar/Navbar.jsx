aba059a33677774b974b370a92a40316f61fc4b8
b <a>
2023-01-31 17:52:03 +0700

    a

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './navbar.css';
+import logo from "../../assets/logo.png"
 const Navbar = () => {
   return (
-    <nav className='App__navbar'>Navbar</nav>
+    <nav className='App__navbar'>
+      <img src={logo} alt="logo" />
+      <ul className='App__navbar-links'>
+        <li >Home</li>
+        <li >What is GPT?</li>
+        <li >Open AI</li>
+        <li >Case Studies</li>
+        <li >Libary</li>
+      </ul>
+      <div className='App__navbar-login'>
+        <a href='#signin' >Sign in</a>
+        <button type='button' >Sign up</button>
+      </div>
+    </nav>
   )
 }
 
