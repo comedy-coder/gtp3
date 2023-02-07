24ed4013ce3edceb9baabe8a6a829187cde1d2c6
b <a>
2023-02-07 16:51:15 +0700

    a

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css';
 import logo from "../../assets/logo.png"
+import {IoClose} from 'react-icons/io5';
+import {GiHamburgerMenu} from 'react-icons/gi'
 const Navbar = () => {
+  const [isToogle , setToogle] = useState(false)
   return (
-    <nav className='App__navbar'>
-      <img src={logo} alt="logo" />
+    <nav className='App__navbar gradient__bg '>
+      <img src={logo} alt="logo"  className='navbar_logo'/>
       <ul className='App__navbar-links'>
         <li >Home</li>
         <li >What is GPT?</li>
@@ -13,9 +16,29 @@ const Navbar = () => {
         <li >Libary</li>
       </ul>
       <div className='App__navbar-login'>
-        <a href='#signin' >Sign in</a>
-        <button type='button' >Sign up</button>
+        <button type='button' className='navbar-signin'>Sign in</button>
+        <button type='button'  className='navbar-signup'>Sign up</button>
+
       </div>
+      <div className='App__navbar-smallscreen'>
+      { isToogle === false ? <GiHamburgerMenu fontSize={27} onClick = {() => {setToogle(!isToogle)}}/> :
+       <IoClose fontSize={27}  onClick = {()=>{setToogle(!isToogle)}}/>}
+      { isToogle && (
+        <div className= 'App__navbar-smallscreen-layout'>
+           
+      <ul className='App__navbar-links-smallscreen'>
+        <li >Home</li>
+        <li >What is GPT?</li>
+        <li >Open AI</li>
+        <li >Case Studies</li>
+        <li >Libary</li>
+      </ul>
+    
+
+      </div>
+      )}
+      </div>
+      
     </nav>
   )
 }
